8a190bc155a72dc3da6371a9cc36f33a2653bfa9
zhaoyang <1182693928@qq.com>
2020-12-14 09:44:19 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### 数据库练习学习
+> 练习使用SQL语句，使用Java代码进行连接数据库
+- [x] [Sqlite3](./SQLite3README.md)
---
+++ src/main/java/cn/zhaosunny/sqlite3/Main.java
@@ -0,0 +1,11 @@
+package cn.zhaosunny.sqlite3;
+
+public class Main {
+    /**
+     * SQL的使用
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
