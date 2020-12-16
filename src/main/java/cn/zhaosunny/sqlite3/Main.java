465e8ad161a2b640075d336b6ae439cc43564577
zhaoyang <1182693928@qq.com>
2020-12-16 17:45:55 +0800

    Training

--- src/main/java/cn/zhaosunny/sqlite3/Main.java
+++ src/main/java/cn/zhaosunny/sqlite3/Main.java
@@ -1,6 +1,9 @@
 package cn.zhaosunny.sqlite3;
 
 
+import org.sqlite.SQLiteConnection;
+import org.sqlite.SQLiteDataSource;
+import org.sqlite.jdbc4.JDBC4PreparedStatement;
 import org.sqlite.jdbc4.JDBC4ResultSet;
 
 import java.io.File;
@@ -16,31 +19,43 @@ public class Main {
     }
 
     public static void readDb() throws SQLException {
-        Connection connection = null;
+        SQLiteConnection connection = null;
         try {
             String dbPath = "jdbc:sqlite:D:/AWebProjects/SQLStudy/databases/user.db";
-            connection = DriverManager.getConnection(dbPath);
+            connection =
+                    connection = ((SQLiteConnection) DriverManager.getConnection(dbPath));
             System.out.println("连接成功");
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("select * from user;");
             List<Map<String, Object>> list = new ArrayList<>();
             System.out.println(Arrays.toString(((JDBC4ResultSet) resultSet).cols));
-            while (resultSet.next()) {
-                Map<String, Object> map = new HashMap<>();
-                String[] colsMeta = ((JDBC4ResultSet) resultSet).colsMeta;
-                for (int i = 0; i < colsMeta.length; i++) {
-                    map.put(colsMeta[i], resultSet.getObject(colsMeta[i]));
-                }
-                list.add(map);
-                System.out.println(map);
-            }
+            printResultSet(list, resultSet);
+            System.out.println("------------------------------");
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "select name, age from user where name = ?");
+           // preparedStatement.setString(1, "name");
+            preparedStatement.setString(1, "张三");
 
+            ResultSet resultSet1 = preparedStatement.executeQuery();
+            printResultSet(list, resultSet1);
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         } finally {
             if (connection != null && !connection.isClosed()) {
                 connection.close();
             }
+        }
+    }
+
+    private static void printResultSet(List<Map<String, Object>> list, ResultSet resultSet1) throws SQLException {
+        while (resultSet1.next()) {
+            Map<String, Object> map = new HashMap<>();
+            String[] colsMeta = ((JDBC4ResultSet) resultSet1).colsMeta;
+            for (int i = 0; i < colsMeta.length; i++) {
+                map.put(colsMeta[i], resultSet1.getObject(colsMeta[i]));
+            }
+            list.add(map);
+            System.out.println(map);
         }
     }
 
